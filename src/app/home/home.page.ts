176caa4c0d29cc7f172159778a7ec266244f0007
Sofi Berumen <sberumen@redlab.com.mx>
2019-04-04 18:05:13 -0600

    exma

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  datos:any;
 
+  text: string;
+
+  constructor(private http: HttpClient, public modalController: ModalController){
+
+  }
+
+  consulta(){//webservice
+
+    this.http.get("https://api.github.com/search/repositories?q="+this.text).subscribe((data:any)=>{
+      console.log("trae: ", data);
+      this.datos=data.items;
+    });//obtener los usuarios de la pagina de jsnholder
+  }
+
+  //async abrir_modal( jaja : any){
+  //  const modal = await this.modalController.create({
+      //component: ModalPage,
+    //  componentProps: { dat: jaja }
+  //  });
+    //await modal.present();
+//  }
+
+}
+
+interface tetris{
+  Archivo?: string;
+  Avatar?: string;
+  Id?: string;
 }
